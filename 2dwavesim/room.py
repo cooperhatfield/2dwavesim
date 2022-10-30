901081c4180f992d1fa81ad6f7e490ae43bf2058
cooperhatfield <47254427+cooperhatfield@users.noreply.github.com>
2022-10-30 13:50:06 -0700

    Add code to run simulation

--- 2dwavesim/room.py
+++ 2dwavesim/room.py
@@ -22,6 +22,7 @@ class Room:
 		self.walls = walls
 		self.func_sources = []
 		self.data_sources = []
+		self.runs = []
 
 	def add_source_func(self, loc, func):
 		true_loc = Coordinate(loc.x // self.point_spacing, loc.y // self.point_spacing)
@@ -49,9 +50,31 @@ class Room:
 	def run(self, dt, t_final):
 		'''Solve the system using a finite differences solver, and return the solved system.
 		'''
+		time_steps = np.arange(0, t_final, dt)
+		room_data = np.zeros((*self.room_points[0].shape(), len(time_steps)), dtype=float)
 
+		for t in range(1, len(time_steps)-1):
+			room_data[:,:,t] = np.multiply(u[:,:,t], self.mask_points)
+			D2x = room_data[:-2,1:-1,t] - 2 * room_data[1:-1,1:-1,t] + room_data[2:,1:-1,t]
+			D2y = room_data[1:-1,:-2,t] - 2 * room_data[1:-1,1:-1,t] + room_data[1:-1,2:,t]
+			room_data[1:-1,1:-1,t+1] = self.wavespeed * (D2x + D2y) + 2 * room_data[1:-1,1:-1,t] + (self.attenuation - 1) * room_data[1:-1,1:-1,t-1]
+			for source in self.func_sources:
+				loc = source[1]
+				source_func = source[2]
+				room_data[loc.x, loc.y, t+1] = source_func(time_steps[t])
+			for source in self.data_sources:
+				loc = source[1]
+				source_data = source[2]
+				room_data[loc.x, loc.y, t+1] = source_data[t]
+		room_data /= (1 + self.attenuation)
 
-
+		run_data = {'time params': {'dt': dt,
+									't_final': t_final},
+					'walls': self.walls,
+					'sources': self.func_sources +  self.data_soures,
+					'results': room_data
+					}
+		self.runs.append(run_data)
 
 class Coordinate:
 	def __init__(self, x, y):
