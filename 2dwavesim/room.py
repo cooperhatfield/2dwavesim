1882b625abc2b0a6503573d782e3889c9c81b2e5
cooperhatfield <47254427+cooperhatfield@users.noreply.github.com>
2022-10-30 18:51:06 -0700

    Update documentation

--- 2dwavesim/room.py
+++ 2dwavesim/room.py
@@ -8,12 +8,12 @@ class Room:
 			ds: (float) size of unit step in space
 			width: (float) width of room, rounded down to nearest multiple of ds
 			height: (float) height of room, rounded down to nearest multiple of ds
-			physics_params: (dict{str:float}) dictionary of physics params for the
-				system. Contains 'wavespeed': speed of waves in medium (float) and
-				'attenuation': attenuation of waves in medium (float).
 		Keyword params:
 			walls: (Wall) List of wall objects containing position and absorption 
 				data.
+			physics_params: (dict{str:float}) dictionary of physics params for the
+				system. Contains 'wavespeed': speed of waves in medium (float) and
+				'attenuation': attenuation of waves in medium (float).
 		'''
 		self.room_points = np.meshgrid(np.arange(0, width, ds), np.arange(0, height, ds))
 		self.mask_points = np.ones(self.room_points[0].shape)
@@ -26,10 +26,12 @@ class Room:
 		self.runs = []
 
 	def add_source_func(self, loc, func):
+		'''Add a source which is based on a function in time. `loc` is the coordinate in the room.'''
 		true_loc = Coordinate(int(loc.x // self.point_spacing), int(loc.y // self.point_spacing))
 		self.func_sources.append((loc, true_loc, func))
 
 	def add_source_data(self, loc, data):
+		'''Add a source which is based on a list of values. `loc` is the coordinate in the room.'''
 		true_loc = Coordinate(int(loc.x // self.point_spacing), int(loc.y // self.point_spacing))
 		self.data_sources.append((loc, true_loc, data))
 
@@ -37,6 +39,9 @@ class Room:
 		self.walls = self.walls + walls
 
 	def create_mask(self):
+		'''Create the wall mask based on all current walls. This uses a modified version of the 
+		Bressenham algorithm for rasterizing the walls to pixels on the grid.
+		'''
 		def bressenham_ABC(p0, p1):
 			A = p1.y - p0.y
 			B = -(p1.x - p0.x)
@@ -54,6 +59,8 @@ class Room:
 
 	def run(self, dt, t_final):
 		'''Solve the system using a finite differences solver, and return the solved system.
+		Make sure the numerical stability is maintained by ensuring (wavespeed)*dt/ds<=1.
+		`dt` is the time step. `t_final` is the time limit on the simulation.
 		'''
 		wave_constant = (self.wavespeed * dt / self.point_spacing)**2
 		damp_constant = self.attenuation * dt / 2
