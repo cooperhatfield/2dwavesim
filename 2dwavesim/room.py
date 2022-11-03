a756ee0b51f87249e08a540dda9f95857de06bb4
cooperhatfield <47254427+cooperhatfield@users.noreply.github.com>
2022-11-03 12:09:10 -0700

    Move animate function to new module

--- 2dwavesim/room.py
+++ 2dwavesim/room.py
@@ -115,38 +115,3 @@ class Wall:
 		self.endpoint1 = Coordinate(endpoint1[0], endpoint1[1])
 		self.endpoint2 = Coordinate(endpoint2[0], endpoint2[1])
 		self.transmission = transmission
-
-def animate(data, name, *, frame_space=10, walls=[]):
-	'''Create an animation of the data.
-	`data`: 3D array of waveform over time
-	`name`: name of output file (if saving is enabled)
-	`frame_space`: temporal resolution of resulting animation. Make sure this isn't too small!
-	`walls`: optionally include the walls in the animation. They won't be visible if this isn't included.
-	'''
-	import matplotlib.animation as animation
-	import matplotlib.pyplot as plt
-	import matplotlib as m
-
-	frame_space = frame_space
-	frame_max = data.shape[-1]
-	fig, [ax, cax] = plt.subplots(1, 2, gridspec_kw={'width_ratios':[10,1]})
-
-	maximum = np.amax(data)
-	minimum = np.amin(data)
-
-	norm = m.colors.Normalize(vmin=minimum, vmax=maximum)
-	cb1 = m.colorbar.ColorbarBase(cax, norm=norm, orientation='vertical')
-
-	ims = []
-	for i in range(0, frame_max, frame_space):
-		im = ax.imshow(data[:,:,i], vmin=minimum, vmax=maximum, animated=True)
-		wallims = []
-		for wall in walls:
-			wallim, = ax.plot([wall.endpoint1.x, wall.endpoint2.x], [wall.endpoint1.y, wall.endpoint2.y], color='black', linewidth=1, alpha=1-wall.transmission)
-			wallims.append(wallim)
-
-		ims.append([im] + wallims)
-
-	ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
-	plt.show()
-	#ani.save(f'D:\\Repos\\2dwavesim\\2dwavesim\\{name}.webp', writer=animation.PillowWriter(fps=24))
